9f684421d77028e1789c342213bd63bb362f1d91
Mateusz Neumann <mateusz.neumann@ntti3.io>
2019-06-04 12:59:10 +0200

    Update address of Tor node used by Privoxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 exec 2>&1
-if [[ -n ${PRIVOXY_INSTANCE} && "0" != ${PRIVOXY_INSTANCE} ]] ; then
-  sed "s/^forward-socks5t\([^t]\+\)tor:9050\(.*\)/forward-socks5t\1tor:1905${PRIVOXY_INSTANCE}\2/" /etc/privoxy/config.template > /etc/privoxy/config
+if [ -n ${PRIVOXY_INSTANCE} ] ; then
+  sed "s/^forward-socks5t\([^t]\+\)tor:9050\(.*\)/forward-socks5t\1tor-${PRIVOXY_INSTANCE}:9050\2/" /etc/privoxy/config.template > /etc/privoxy/config
 else
   cp etc/privoxy/config.template /etc/privoxy/config
 fi
